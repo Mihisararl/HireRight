e03c03c02a6118730ad19e92060d3dec785a8481
Mihisara <155085512+Mihisararl@users.noreply.github.com>
2026-04-17 10:57:45 +0530

    Updated the web-frontend submodule to reflect the latest changes.

--- backend/models/User.js
+++ backend/models/User.js
@@ -46,6 +46,7 @@ userSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
+  
 });
 
 export default mongoose.model('User', userSchema);
